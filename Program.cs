a492f8c360b1a88516207e7117db6a8b9a68d3f9
StevanFreeborn <sfreeborn7@gmail.com>
2022-09-28 21:27:46 -0500

    add example with and without sdk

--- Program.cs
+++ Program.cs
@@ -1,16 +1,95 @@
 ﻿using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+using Onspring.API.SDK;
+using Onspring.API.SDK.Models;
 
-var apiKey = "NEEDS TO BE SET";
-var requestUri = "https://api.onspring.com/Files";
-var fileName = "test-attachment.txt";
-var filePath = @$"./{fileName}";
-var contentType = "plain/txt";
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
 
-var stream = File.OpenRead(filePath);
-var streamContent = new StreamContent(stream);
-streamContent.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+var result = await SaveFile(configuration.GetSection("ApiKey").Value, 123, 6989, "./test-attachment.txt");
+Console.WriteLine(result.ToString());
+await SaveFileUsingSdk();
+await SaveFileWithoutUsingSdk();
 
-var multiPartContent = new MultipartFormDataContent
+async Task<int> SaveFile(string apiKey, int recordId, int fieldId, string filePath)
+{
+
+    if (File.Exists(filePath) && !string.IsNullOrEmpty(apiKey))
+    {
+
+        var requestUri = "https://api.onspring.com/Files";
+
+        var fileName = Path.GetFileName(filePath);
+
+        //var contentType = System.Web.MimeMapping.GetMimeMapping(fileName);
+        var contentType = "plain/text";
+
+        var stream = File.OpenRead(filePath);
+
+        var streamContent = new StreamContent(stream);
+
+        streamContent.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+
+
+
+        var multiPartContent = new MultipartFormDataContent
+
+        {
+
+            { streamContent, "File", fileName },
+
+            { new StringContent(recordId.ToString()), "RecordId" },
+
+            { new StringContent(fieldId.ToString()), "FieldId" },
+
+            { new StringContent(""), "Notes" },
+
+        };
+
+
+
+        var httpRequest = new HttpRequestMessage(HttpMethod.Post, requestUri)
+
+        {
+
+            Content = multiPartContent,
+
+        };
+
+
+
+        httpRequest.Headers.Add("x-apikey", apiKey);
+
+        httpRequest.Headers.Add("x-api-version", "2");
+
+        var httpClient = new HttpClient();
+
+        var httpResponse = await httpClient.SendAsync(httpRequest);
+
+        return (httpResponse.StatusCode == System.Net.HttpStatusCode.Created) ? 1 : 0;
+
+    }
+
+    return 0;
+
+}
+
+
+// Not using SDK
+async Task SaveFileWithoutUsingSdk()
+{
+    var apiKey = configuration.GetSection("ApiKey").Value;
+    var requestUri = "https://api.onspring.com/Files";
+    var fileName = "test-attachment.txt";
+    var filePath = @$"./{fileName}";
+    var contentType = "plain/txt";
+
+    var stream = File.OpenRead(filePath);
+    var streamContent = new StreamContent(stream);
+    streamContent.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+
+    var multiPartContent = new MultipartFormDataContent
 {
     { streamContent, "file", fileName },
     { new StringContent("123"), "RecordId" },
@@ -18,15 +97,43 @@ var multiPartContent = new MultipartFormDataContent
     { new StringContent("This is a test"), "Notes" },
 };
 
-var httpRequest = new HttpRequestMessage(HttpMethod.Post, requestUri)
+    var httpRequest = new HttpRequestMessage(HttpMethod.Post, requestUri)
+    {
+        Content = multiPartContent,
+    };
+
+    httpRequest.Headers.Add("x-apikey", apiKey);
+
+    var httpClient = new HttpClient();
+
+    var httpResponse = await httpClient.SendAsync(httpRequest);
+
+    Console.WriteLine(httpResponse.StatusCode);
+}
+
+// Using SDK
+async Task SaveFileUsingSdk()
 {
-    Content = multiPartContent,
-};
+    var apiKey = configuration.GetSection("ApiKey").Value;
+    var baseUrl = "https://api.onspring.com";
 
-httpRequest.Headers.Add("x-apikey", apiKey);
+    var fileName = "test-attachment.txt";
+    var filePath = @$"./{fileName}";
+    var contentType = "plain/txt";
 
-var httpClient = new HttpClient();
+    var client = new OnspringClient(baseUrl, apiKey);
 
-var httpResponse = await httpClient.SendAsync(httpRequest);
+    var request = new SaveFileRequest
+    {
+        RecordId = 123,
+        FieldId = 6989,
+        Notes = "This is a test",
+        FileName = fileName,
+        FileStream = File.OpenRead(filePath),
+        ContentType = contentType,
+    };
 
-Console.WriteLine(httpResponse.StatusCode);
+    var response = await client.SaveFileAsync(request);
+
+    Console.WriteLine(response.StatusCode);
+}
